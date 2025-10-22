7ef22ee68c29ff3cf12840c9cb38cf0986ffbf1a
sbx_user1051 <sbx_user1051@169.254.22.217>
2025-10-22 15:36:20 +0000

    Wed Oct 22 15:36:20 UTC 2025

--- test/solution_tests/CHK/test_chk_solution.py
+++ test/solution_tests/CHK/test_chk_solution.py
@@ -10,18 +10,28 @@ class TestSum:
             ("B", 30),
             ("C", 20),
             ("D", 15),
+            ("E", 40),
             ("AA", 100),
             ("AAA", 130),
             ("AAAA", 180),
-            ("AAAAA", 130 + 50 * 2),
-            ("AAAAAA", 130 * 2),
+            ("AAAAA", 200),
+            ("AAAAAA", 250),
+            ("AAAAAAA", 300),
+            ("AAAAAAAA", 330),
             ("BB", 45),
             ("BBB", 75),
             ("BBBB", 90),
+            ("EE", 80),
+            ("EEB", 80),
+            ("EEEB", 120),
+            ("EEBB", 110),
+            ("EEEEBB", 160),
             ("ABCD", 115),
+            ("ABCDE", 155),
             ("", 0),
             ("INVALID", -1),
         ],
     )
-    def test_checkout(self, items, expected):
+    def test_checkout(self, items: str, expected: int) -> None:
         assert CheckoutSolution().checkout(items) == expected
+
