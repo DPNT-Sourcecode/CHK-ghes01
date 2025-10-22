7cdcb296da16255c40e9d3af69d7138cd5394ab7
sbx_user1051 <sbx_user1051@169.254.22.217>
2025-10-22 17:23:39 +0000

    Wed Oct 22 17:23:39 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -80,7 +80,7 @@ DEFAULT_BASE_PRICES = {
     "H": 10,
     "I": 35,
     "J": 60,
-    "K": 80,
+    "K": 70,
     "L": 90,
     "M": 15,
     "N": 40,
@@ -88,14 +88,14 @@ DEFAULT_BASE_PRICES = {
     "P": 50,
     "Q": 30,
     "R": 50,
-    "S": 30,
+    "S": 20,
     "T": 20,
     "U": 40,
     "V": 50,
     "W": 20,
-    "X": 90,
-    "Y": 10,
-    "Z": 50,
+    "X": 17,
+    "Y": 20,
+    "Z": 21,
 }
 
 
@@ -271,4 +271,5 @@ class CheckoutSolution:
         )
 
         return total_cost
+
 
