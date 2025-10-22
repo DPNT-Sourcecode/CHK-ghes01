a34baa0f698bb2ffd5e6a866bd19c876daf43657
sbx_user1051 <sbx_user1051@169.254.22.217>
2025-10-22 15:12:30 +0000

    Wed Oct 22 15:12:30 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,20 @@
-
 class CheckoutSolution:
+    def calculate_cost(self, sku, num_items):
+        match sku:
+            case "A":
+                offer_cost = num_items // 3
+                return offer_cost * 130 + (num_items % 3) * 50
+            case "B":
+                offer_cost = num_items // 2
+                return offer_cost * 45 + (num_items % 2) * 30
+            case "C":
+                return num_items * 20
+            case "D":
+                return num_items * 15
+            case _:
+                raise ValueError("Invalid SKU")
 
     # skus = unicode string
     def checkout(self, skus):
         raise NotImplementedError()
+
